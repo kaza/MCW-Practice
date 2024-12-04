35dcefe65c2608fd410ece9525a842290eb40bda
Shawaiz Sarfraz <shawaizsarfraz117@gmail.com>
2024-12-05 03:02:41 +0500

    update workflow file

--- config/settings/production.py
+++ config/settings/production.py
@@ -35,27 +35,3 @@ DATABASES['default'].update({
         **DATABASES['default']['OPTIONS']
     }
 })
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
