d43fba87620ab218ee4e68c3e477ebd2b032ff87
Shawaiz Sarfraz <shawaizsarfraz117@gmail.com>
2024-12-05 02:38:35 +0500

    update workfile file

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,15 +10,15 @@ DEBUG = False
 ALLOWED_HOSTS = ['simple-practice.azurewebsites.net']
 
 # Security settings
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_SECONDS = 31536000  # 1 year
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# X_FRAME_OPTIONS = 'DENY'
+# SECURE_HSTS_SECONDS = 31536000  # 1 year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 
 # Static files
 STATIC_URL = '/static/'
@@ -30,10 +30,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Replace CORS_ALLOW_ALL_ORIGINS with specific origins
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "https://simple-practice.azurewebsites.net/",
-]
+# CORS_ALLOW_ALL_ORIGINS = False
+# CORS_ALLOWED_ORIGINS = [
+#     "https://simple-practice.azurewebsites.net/",
+# ]
 
 
 DATABASES['default'].update({
