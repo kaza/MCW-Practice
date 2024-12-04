4b388798335d4d5e24ae02c44c6291c14a235add
Shawaiz Sarfraz <shawaizsarfraz117@gmail.com>
2024-12-05 01:55:30 +0500

    Add production.py

---
+++ config/settings/production.py
@@ -0,0 +1,62 @@
+from .base import *
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Set DEBUG to False in production
+DEBUG = False
+
+ALLOWED_HOSTS = ['simple-practice.azurewebsites.net']
+
+# Security settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Replace CORS_ALLOW_ALL_ORIGINS with specific origins
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://simple-practice.azurewebsites.net/",
+]
+
+
+DATABASES['default'].update({
+    'OPTIONS': {
+        **DATABASES['default']['OPTIONS']
+    }
+})
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'logs' / 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
